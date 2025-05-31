402f5af1c4d56c284ced4e89c7a1411937b35986
serguntsov <romanmc2203@mail.ru>
2025-05-31 21:36:11 +0300

    Создали собственный текстовый файл с необходимыми нам названиями. Пробуем реализовать их качественную склейку

--- russian_dictionary/convert_utf8.py
+++ russian_dictionary/convert_utf8.py
@@ -11,23 +11,28 @@ def convert_and_compress():
         # Переводим в формат utf-8 и соединяем файлы в единый
         with open(combined_filename, 'w', encoding='utf-8') as combined_out:
             for filename in input_files:
-                subprocess.run(
-                    ['iconv', '-f', 'WINDOWS-1251', '-t', 'UTF-8', filename],
-                    stdout=combined_out,
-                    check=True
-                )
+                # Наш файл читаем как есть
+                if filename == 'my_word.txt':
+                    with open(filename, 'r', encoding='utf-8') as f:
+                        combined_out.writelines(f)
+                else:
+                    subprocess.run(
+                        ['iconv', '-f', 'WINDOWS-1251', '-t', 'UTF-8', filename],
+                        stdout=combined_out,
+                        check=True
+                    )
 
         # Создание архива .gz под wordninja
         gz_filename = combined_filename + '.gz'
         with open(combined_filename, 'rt', encoding='utf-8', errors='replace') as f_in, \
-            gzip.open(gz_filename, 'wt', encoding='utf-8') as f_out:
+             gzip.open(gz_filename, 'wt', encoding='utf-8') as f_out:
             for line in f_in:
                 f_out.write(line)
 
-        print(f"Архив был создан успешно: {gz_filename}") 
+        print(f"Архив был создан успешно: {gz_filename}")
 
     except subprocess.CalledProcessError as e:
-        print(f"Возникла ошибка: {e}")
+        print(f"Ошибка при конвертации: {e}")
 
 if __name__ == '__main__':
     convert_and_compress()
